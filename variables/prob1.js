c0289f34bf9b30da22c860bf271d5ccf6e510179
Bindhu Madavali <bindhumadavali2000@gmail.com>
2023-01-03 08:47:48 +0530

    first commit

---
+++ variables/prob1.js
@@ -0,0 +1,7 @@
+
+
+//print "Masai School" in the console followed by "A Transformation in Education" in next line.
+let school = "Masai School";
+let edc="A Transformation in Education";
+console .log(school);
+console.log(edc);
